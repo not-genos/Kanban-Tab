1e420016d2c37b63d4860191345b2f8f8ddfcd46
Ankush Kashyap <ankumar505@gmail.com>
2024-12-04 00:24:57 +0530

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import KanbanBoard from './components/KanbanTab';
-import { fetchTickets } from './api/tickets';
 import './global.css';
 
 const App = () => {
-    const [tickets, setTickets] = useState([]);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = await fetchTickets();
-            setTickets(data);
-        };
-
-        fetchData();
-    }, []);
-
     return (
         <div>
-            {/* <h1>Kanban Board</h1> */}
-            <KanbanBoard tickets={tickets} />
+            <KanbanBoard />
         </div>
     );
 };
